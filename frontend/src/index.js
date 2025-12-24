a1c1528b2813690518d5d88d773fde890ff71792
Sigma Student <Sigmastudent@gmail.com>
2025-12-24 23:12:36 +0530

    Fix broken landing_page imports in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,29 @@
-import Dashboard from "./pages/dashboard/Dashboard"; // import Dashboard
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Dashboard from "./pages/dashboard/Dashboard";
+// Landing pages
+import HomePage from "./pages/landing/home/HomePage";
+import Signup from "./pages/landing/signup/Signup";
+import Login from "./pages/landing/signup/Login";
+import AboutPage from "./pages/landing/about/AboutPage";
+import ProductPage from "./pages/landing/products/ProductPage";
+import PricingPage from "./pages/landing/pricing/PricingPage";
+import SupportPage from "./pages/landing/support/SupportPage";
+import NotFound from "./pages/landing/NotFound";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Navbar />
     <Routes>
-      {/* Landing Pages */}
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
@@ -12,8 +31,8 @@ root.render(
       <Route path="/product" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
       <Route path="/support" element={<SupportPage />} />
-
-      {/* Kite Dashboard */}
+      <Route path="*" element={<NotFound />} />
+       {/* Kite Dashboard */}
       <Route path="/kite/*" element={<Dashboard />} />
 
       {/* Fallback */}
